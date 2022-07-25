1f83bc9661233599ba4b2fd833e778c80062967c
Sanathchandran <104377886+Sanathchandran@users.noreply.github.com>
2022-07-25 15:45:47 +0530

    initial commit

--- Main.py
+++ Main.py
@@ -5,21 +5,24 @@ class Stack:
         self.size = size
 
     def is_empty(self):
-        # Write code here
+        if len(self.item) ==0
+           return True
 
     def is_full(self):
-        # Write code here
+         if len(self.item) ==self.size:
+           return True
 
     def push(self, data):
         if not self.is_full():
-            # Write code here
+            self.items.appened(data)
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
+            self.items.pop()
 
     def status(self):
-        # Write code here
+        for element in self.items:
+            print(element)
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
